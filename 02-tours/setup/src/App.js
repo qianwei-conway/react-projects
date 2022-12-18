d2e3b0e5d767735ac13465cb2a0198a2ba2049aa
Conway <937700753@qq.com>
2022-12-17 23:25:48 -0800

    02-tours

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+	const [isLoading, setIsLoading] = useState(true);
+	const [tours, setTours] = useState([]);
+
+	const removeTour = (id) => {
+		setTours((tours) => {
+			return tours.filter((tour) => id !== tour.id);
+		});
+	};
+
+	const fetchTours = async () => {
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+
+			setTours(data);
+			setIsLoading(false);
+		} catch (error) {
+			setIsLoading(true);
+			console.log(error);
+		}
+	};
+
+	// you may wondering why we have to use useEffect, why not just write a line to call fetchTours()? Because doing this makes sure that only at the beginning (with empty dependency) can program call fetchTours() function, otherwise in every re-rendering the program will fetch data from API.
+	useEffect(() => {
+		fetchTours(); // have to use {} to mean that the function doesn't return anything
+	}, []);
+
+	return (
+		<main>
+			{tours.length === 0 ? (
+				<div className="title">
+					<h2>no tours left</h2>
+					<button onClick={fetchTours} className="btn">
+						refresh
+					</button>
+				</div>
+			) : isLoading ? (
+				<Loading />
+			) : (
+				<Tours tours={tours} removeTour={removeTour} />
+			)}
+		</main>
+	);
 }
 
-export default App
+export default App;
