df4bff475f918b28b40be3341a92a68fee098011
Conway <937700753@qq.com>
2022-12-18 21:42:13 -0800

    09-color-generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,48 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-	return <h2>color generator project</h2>;
+	const [color, setColor] = useState("");
+	const [error, setError] = useState(false);
+	const [colorList, setColorList] = useState(new Values("#f15025").all(5));
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		try {
+			setError(false);
+			let newColorList = new Values(color).all(5);
+			setColorList(newColorList);
+		} catch (error) {
+			setError(true);
+			console.log(error);
+		}
+	}
+
+	return (
+		<>
+			<section className="container">
+				<h3>color generator</h3>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={color}
+						placeholder="#f15025"
+						onChange={(e) => setColor(e.target.value)}
+						className={error ? "error" : null}
+					/>
+					<button className="btn" type="submit">
+						submit
+					</button>
+				</form>
+			</section>
+			<section className="colors">
+				{colorList.map((color, index) => {
+					return (
+						<SingleColor key={index} color={color} index={index} />
+					);
+				})}
+			</section>
+		</>
+	);
 }
 
 export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,36 @@
 import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
-const SingleColor = () => {
-	return <h4>single color</h4>;
+const SingleColor = ({ color, index }) => {
+	const [isCopied, setIsCopied] = useState(false);
+	const { rgb, weight } = color;
+	const hex = rgbToHex(...rgb);
+	const bcg = rgb.join(",");
+
+	// un-display the alert message
+	useEffect(() => {
+		const timeOut = setTimeout(() => {
+			setIsCopied(false);
+		}, 2000);
+
+		// cleanup function
+		return () => clearTimeout(timeOut);
+	}, [isCopied]);
+
+	return (
+		<article
+			className={index > 20 ? "color-light" : null}
+			style={{ backgroundColor: `rgb(${bcg})` }}
+			onClick={() => {
+				setIsCopied(true);
+				navigator.clipboard.writeText(hex);
+			}}
+		>
+			<p className="percent-value">{weight}%</p>
+			<p className="color-value">{hex}</p>
+			{isCopied ? <p className="alert">copied to clipboard</p> : null}
+		</article>
+	);
 };
 
 export default SingleColor;
