671bdab8ae2ba9d44a9ff7ad9e6b0aaa79af4405
Conway <937700753@qq.com>
2022-12-18 16:35:23 -0800

    08-lorem-ipsum

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [amount, setAmount] = useState(1);
+	const [text, setText] = useState([data[0]]);
+
+	function handleClick(e) {
+		e.preventDefault();
+		let count = amount;
+		if (amount <= 0) {
+			count = 0;
+		} else if (amount > data.length) {
+			count = data.length;
+		}
+
+		setText((text) => data.slice(0, count));
+	}
+
+	return (
+		<section className="section-center">
+			<h3>TIRED OF BORING LOREM IPSUM?</h3>
+			<form className="lorem-form">
+				<label htmlFor="amount">paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={amount}
+					onChange={(e) => {
+						setAmount(e.target.value);
+					}}
+				></input>
+				<button className="btn" onClick={handleClick}>
+					generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((par, index) => {
+					return <p key={index}>{par}</p>;
+				})}
+			</article>
+		</section>
+	);
 }
 
 export default App;
