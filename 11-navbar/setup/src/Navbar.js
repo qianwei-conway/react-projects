19baf3451eb76e9f60c9cfaafb0e5e51fb31c03d
Conway <937700753@qq.com>
2022-12-18 23:02:26 -0800

    11-navbar

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,52 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+	const [showLinks, setShowLinks] = useState(true);
 
-export default Navbar
+	return (
+		<div className="nav-center">
+			<div className="nav-header">
+				<img src={logo} alt="logo" />
+				<button
+					className="nav-toggle"
+					onClick={() => setShowLinks(!showLinks)}
+				>
+					<FaBars />
+				</button>
+			</div>
+
+			<div
+				className={
+					showLinks
+						? "links-container show-container"
+						: "links-container"
+				}
+			>
+				<ul className="links">
+					{links.map((link) => {
+						return (
+							<li key={link.id}>
+								<a href={link.url}>{link.text}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+
+			<ul className="social-icons">
+				{social.map((socialIcon) => {
+					return (
+						<li key={socialIcon.id}>
+							<a href={socialIcon.url}>{socialIcon.icon}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+
+export default Navbar;
