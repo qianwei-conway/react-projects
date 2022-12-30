4ad8c2cfb75c8e74eeafd8a96781d719971f0e58
Conway <937700753@qq.com>
2022-12-29 16:15:56 -0800

    13-stripe-submenus

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,43 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+	function openSidebar() {
+		setIsSidebarOpen(true);
+	}
+	function closeSidebar() {
+		setIsSidebarOpen(false);
+	}
+	function openSubmenu() {
+		setIsSubmenuOpen(true);
+	}
+	function closeSubmenu() {
+		setIsSubmenuOpen(false);
+	}
+
+	return (
+		<AppContext.Provider
+			value={
+				(isSidebarOpen,
+				isSubmenuOpen,
+				openSidebar,
+				openSubmenu,
+				closeSubmenu,
+				closeSidebar)
+			}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
