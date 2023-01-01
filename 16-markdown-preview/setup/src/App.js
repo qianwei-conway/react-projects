ab0ae3a2dbbdb4c709ae6f77a28635a49af37bde
Conway <937700753@qq.com>
2022-12-31 18:04:38 -0800

    16-markdown-preview

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -1,8 +1,31 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+	const [markdown, setMarkdown] = useState(`## Markdown Preview
+> Designed by Qianwei
+#### Hello World
+Lorem, ipsum dolor sit amet consectetur **adipisicing** elit. Minima asperiores iste blanditiis. Pariatur adipisci mollitia aspernatur
+\`\`\`
+alert()
+\`\`\`
+[Go google](https://www.google.com)
+`);
+
+	return (
+		<main>
+			<section className="markdown">
+				<textarea
+					className="input"
+					value={markdown}
+					onChange={(e) => setMarkdown(e.target.value)}
+				></textarea>
+				<article className="result">
+					<ReactMarkdown>{markdown}</ReactMarkdown>
+				</article>
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
