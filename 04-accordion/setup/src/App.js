39e2834166210fc6219f4a28451bdb42ec5c0762
qianwei-yin <qianweiyin22@gmail.com>
2023-01-15 23:02:41 -0800

    accordion update

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import data from "./data";
-import Question from "./Question";
+import React from 'react';
+import data from './data';
+import Question from './Question';
 function App() {
 	return (
-		<div className="container">
-			<h3>Questions And Answers About Login</h3>
+		<div className="accordion">
+			<h3>Common Questions And Answers</h3>
 			<div>
 				{data.map((question) => (
 					<Question key={question.id} question={question} />
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Question = ({ question }) => {
 	const { title, info } = question;
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="question">
+		<div className={`${isOpen ? 'open' : null} question`}>
 			<header>
 				<h4>{title}</h4>
 				<button className="btn" onClick={() => setIsOpen(!isOpen)}>
